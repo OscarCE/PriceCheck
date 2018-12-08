b383dbadd60100a8835e3609a931c8953032fa21
OscarCE <45036129+OscarCE@users.noreply.github.com>
2018-12-09 00:01:53 +1100

    coles actualizado

--- serverdev.ts
+++ serverdev.ts
@@ -60,22 +60,147 @@ app.all('/api/type2/*', (req, res) =>
       break;
   }
 
+  const payload = `<devicefingerprint>
+	<Attributes>
+<Attribute>
+		<Name>OS</Name>
+		<Value><![CDATA[Mac OSX (Version Unknown) Unknown]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>TIME_ZONE</Name>
+		<Value><![CDATA[11]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>SCREEN_ATTRIBUTES</Name>
+		<Value><![CDATA[24|1280|800|1280|777]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>USER_AGENT</Name>
+		<Value><![CDATA[mozilla/5.0 (macintosh; intel mac os x 10_14_1) applewebkit/537.36 (khtml, like gecko) chrome/70.0.3538.110 safari/537.36|MacIntel]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>BROWSER_VER</Name>
+		<Value><![CDATA[Chrome 70]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>LANGUAGES</Name>
+		<Value><![CDATA[BRW=en-AU|SYS=|USR=]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>FLASH_VER</Name>
+		<Value><![CDATA[Not Installed]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>SILVERLIGHT</Name>
+		<Value><![CDATA[Not Installed]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>PLUGINS</Name>
+		<Value><![CDATA[Plugin 0: Chrome PDF Plugin; Portable Document Format; internal-pdf-viewer; (Portable Document Format; application/x-google-chrome-pdf; pdf). Plugin 1: Chrome PDF Viewer; ; mhjfbmdgcfjbbpaeojofohoefgiehjai; (; application/pdf; pdf). Plugin 2: Native Client; ; internal-nacl-plugin; (Native Client Executable; application/x-nacl; ) (Portable Native Client Executable; application/x-pnacl; ). ]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>FONTS</Name>
+		<Value><![CDATA[Arial Black|Courier New|Menlo|Papyrus|Plantagenet Cherokee|Tahoma|Trebuchet MS|Webdings]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>TIME_SKEW</Name>
+		<Value><![CDATA[]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>GEOIP_HTML5</Name>
+		<Value><![CDATA[undefined]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>CANVAS</Name>
+		<Value><![CDATA[396d29a426c8717b7a2514f4acd640f68a902e0af96399119f32278d856b79cf]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>LOCAL_IP</Name>
+		<Value><![CDATA[Local IP is not defined]]></Value>
+	</Attribute>
+<Attribute>
+		<Name>HASH</Name>
+		<Value><![CDATA[e85142af8aef046709b146add7998b830c0dd26e1e01d9e5535bf3fcbdc6f4ef]]></Value>
+	</Attribute>
+	</Attributes>
+</devicefingerprint>`;
+
+  // new cookie jar
+  const j = request.jar();
   // Send the request.
   request(host, {
     headers: {
       'User-Agent': config.userAgent,
     },
+    jar: j,
     method: req.method,
     body: req.method === 'POST' ? req.body : undefined,
     json: true,
-  }).then((response) =>
-  {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(response));
-  }).catch((error) =>
-  {
-    res.end('Error 3');
-  });
+  }, (error, response, body) =>
+    {
+      if (error)
+      {
+        res.end('Error 3.1');
+        return;
+      }
+
+      if (req.method === 'POST')
+      {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(body));
+      } else
+      {
+        const isHtml = /<html>/gmi.test(body);
+
+        // If the response is an HTML, get the final cookie.
+        if (isHtml)
+        {
+          request('https://shop.coles.com.au/fp/awasah/',
+            {
+              headers: {
+                'Content-type': 'application/xml',
+              },
+              jar: j,
+              method: 'POST',
+              body: payload,
+            }, (error2, response2, body2) =>
+            {
+              if (error2)
+              {
+                res.end('Error 3.2');
+                return;
+              }
+              // Now that we have the final cookie, send the request again
+              // to get the JSON we want.
+              request(host,
+                {
+                  headers: {
+                    'User-Agent': config.userAgent,
+                  },
+                  jar: j,
+                  method: 'GET',
+                  json: true,
+                }, (error3, response3, body3) =>
+                {
+                  if (error3)
+                  {
+                    res.end('Error 3.3');
+                    return;
+                  }
+                  res.setHeader('Content-Type', 'application/json');
+                  res.end(JSON.stringify(body3));
+                },
+              );
+            },
+          );
+        }
+        else
+        {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(response));
+        }
+      }
+    });
 });
 
 // -------------------------------------------------------------------------
